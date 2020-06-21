600d72c48ccf01bcc331dc5dc7935598472489c4
Werner Finke <wernerfinke@Werners-MBP.fritz.box>
2020-06-21 11:43:19 +0200

    11. Image Version.

--- Test-image/sketch.js
+++ Test-image/sketch.js
@@ -2,11 +2,12 @@ var img = []; // Declare variable 'img'.
 
 function setup() {
   createCanvas(400, 400);
- /* for (var i = 0; i < 16; i++) {
+  for (var i = 0; i < 16; i++) {
   	var stg = "images/Memory" + (i+1) + ".jpg";
     img[i] = loadImage(stg); // Load the image
   } 
- */
+ 
+ /*
   img[0] = loadImage("images/Memory1.jpg"); // Load the image
   img[1] = loadImage("images/Memory2.jpg"); // Load the image
   img[2] = loadImage("images/Memory3.jpg"); // Load the image
@@ -15,13 +16,13 @@ function setup() {
   img[5] = loadImage("images/Memory6.jpg"); // Load the image
   img[6] = loadImage("images/Memory7.jpg"); // Load the image
   img[7] = loadImage("images/Memory8.jpg"); // Load the image
-  
+  */
 }
 
 function draw() {
-	
+
   // Displays image
-  for (var i = 0; i < 2; i++) {
+  for (var i = 0; i < 4; i++) {
    for (var j = 0; j < 4; j++) {
     image(img[4*i + j], i * 100, j * 100);
    }
