4ab031167deaf1d43fd3b290deef63db111a7348
Werner Finke <wer.finke@t-online.de>
2020-06-21 15:02:40 +0200

    12. Image Version.

--- Test-image/sketch.js
+++ Test-image/sketch.js
@@ -1,36 +1,53 @@
 var img = []; // Declare variable 'img'.
+var firstImage = 0;
+var mouseIstGedrueckt = false;
 
+  
 function setup() {
   createCanvas(400, 400);
   for (var i = 0; i < 16; i++) {
   	var stg = "images/Memory" + (i+1) + ".jpg";
     img[i] = loadImage(stg); // Load the image
   } 
- 
- /*
-  img[0] = loadImage("images/Memory1.jpg"); // Load the image
-  img[1] = loadImage("images/Memory2.jpg"); // Load the image
-  img[2] = loadImage("images/Memory3.jpg"); // Load the image
-  img[3] = loadImage("images/Memory4.jpg"); // Load the image
-  img[4] = loadImage("images/Memory5.jpg"); // Load the image
-  img[5] = loadImage("images/Memory6.jpg"); // Load the image
-  img[6] = loadImage("images/Memory7.jpg"); // Load the image
-  img[7] = loadImage("images/Memory8.jpg"); // Load the image
-  */
+
+  firstImage = round(random(0,16));
+
+  // Displays image
+
+  for (var i = 0; i < 4; i++) {
+   for (var j = 0; j < 4; j++) {
+    image(img[firstImage], i * 100, j * 100);
+   }
+  } 
 }
 
 function draw() {
 
-  // Displays image
-  for (var i = 0; i < 4; i++) {
-   for (var j = 0; j < 4; j++) {
-    image(img[4*i + j], i * 100, j * 100);
+  if (mouseIstGedrueckt) { 
+ 
+   // Displays image
+   for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 4; j++) {
+     image(img[4*i + j], i * 100, j * 100);
+    } 
    }
-  } /*
-    image(img[0], 0, 0);
-    image(img[4], 0, 100);
-    image(img[1], 100, 100);
-    image(img[2], 200, 200);
-    image(img[3], 300, 300);
-  */
+  } else {
+    
+   firstImage = round(random(0,16));
+    // Displays image
+
+   for (var i = 0; i < 4; i++) { 
+    for (var j = 0; j < 4; j++) {
+     image(img[firstImage], i * 100, j * 100);
+    }
+   }	
+  }
+}
+
+function mousePressed() {
+  mouseIstGedrueckt = true;
+}
+
+function mouseReleased() {
+  mouseIstGedrueckt = false;
 }
