0c43cfdb26e8bc6fbeddca0a7c3dd34ea85d34d2
Werner Finke <wernerfinke@Werners-MBP.fritz.box>
2020-06-21 11:36:55 +0200

    10. Image Version.

--- Test-image/sketch.js
+++ Test-image/sketch.js
@@ -12,20 +12,24 @@ function setup() {
   img[2] = loadImage("images/Memory3.jpg"); // Load the image
   img[3] = loadImage("images/Memory4.jpg"); // Load the image
   img[4] = loadImage("images/Memory5.jpg"); // Load the image
+  img[5] = loadImage("images/Memory6.jpg"); // Load the image
+  img[6] = loadImage("images/Memory7.jpg"); // Load the image
+  img[7] = loadImage("images/Memory8.jpg"); // Load the image
   
 }
 
 function draw() {
+	
   // Displays image
-/*  for (var i = 0; i < 4; i++) {
+  for (var i = 0; i < 2; i++) {
    for (var j = 0; j < 4; j++) {
     image(img[4*i + j], i * 100, j * 100);
    }
-  } */
+  } /*
     image(img[0], 0, 0);
     image(img[4], 0, 100);
     image(img[1], 100, 100);
     image(img[2], 200, 200);
     image(img[3], 300, 300);
-  
+  */
 }
