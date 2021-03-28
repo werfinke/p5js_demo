2166f1cb3fecd7f0d852f4fb932270223bb6f029
werfinke <67070348+werfinke@users.noreply.github.com>
2021-03-28 11:47:17 +0200

    Add test2.ts

---
+++ my_mongo_p5js/test2.ts
@@ -0,0 +1,11 @@
+//
+console.log(import.meta.url);
+
+console.log(Deno.mainModule);
+
+console.log(Deno.execPath());
+
+const path2 = Deno.args[0];
+const path = Deno.cwd();
+console.log(path);
+console.log(path2);
